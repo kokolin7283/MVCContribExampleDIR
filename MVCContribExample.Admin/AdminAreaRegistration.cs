be5366b2aa19a6f4c94d9df9b10f9a2e8f49bfac
國祥 林 <koko7283@gmail.com>
2020-04-12 18:44:52 +0800

    加入專案檔案。

---
+++ MVCContribExample.Admin/AdminAreaRegistration.cs
@@ -0,0 +1,31 @@
+﻿using MvcContrib.PortableAreas;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVCContribExample.Admin
+{
+    public class AdminAreaRegistration : PortableAreaRegistration
+    {
+        public override string AreaName
+        {
+            get
+            {
+                return "Admin";
+            }
+        }
+
+        public override void RegisterArea(AreaRegistrationContext context, IApplicationBus bus)
+        {
+            context.MapRoute(
+                    "Admin_default",
+                    "Admin/{controller}/{action}/{id}",
+                    new {action = "Index",id= UrlParameter.Optional},
+                    new[] {"MVCContribExample.Admin.Controllers"}
+                );
+            RegisterAreaEmbeddedResources();
+        }
+    }
+}
